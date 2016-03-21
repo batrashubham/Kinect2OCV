2b4ca17076e88221100cdb598683ebd334ab6bfa
Shubham Batra <batrashubham@live.com>
2016-03-21 16:16:55 +0530

    Update main.cpp

--- Sample/main.cpp
+++ Sample/main.cpp
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  *
  */
-
+ 
 #include<iostream>
 #include "KinectStreamsMat.h"
 
@@ -28,14 +28,17 @@ int main() {
 		return hr;
 	}
 
-	cv::Mat depthImage,colorImage;
+	cv::Mat depthImage,colorImage,irImage;
 
 	while (true) {
 		//Get a single depth frame from kinect
-		depthImage = kinectStream->getDepthFrame();
+		depthImage = kinectStream->getFrame(KS_DEPTH);
 
 		//Get a single color frame from kinect
-		colorImage = kinectStream->getColorFrame();
+		colorImage = kinectStream->getFrame(KS_COLOR);
+		
+		//Get a single IR frame from Kinect
+		irImage = kinectStream->getFrame(KS_IR);
 
 		//ensure depthImage has data
 		if (depthImage.data)
@@ -43,7 +46,11 @@ int main() {
 
 		//ensure colorImage has data
 		if(colorImage.data)
-		   cv::imshow("color", colorImage);
+		   cv::imshow("Color", colorImage);
+	   
+	   //ensure irImage has data
+	   if(irImage.data)
+		   cv::imshow("Infrared",irImage);
 
 		//exit if escape key is pressed
 		if (cv::waitKey(20) == 27) {
