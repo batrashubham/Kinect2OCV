cb01ec88273403b9d29351cfa38054ee60f19469
Shubham Batra <batrashubham@live.com>
2016-03-21 16:15:12 +0530

    Update KinectStreamsMat.cpp

--- src/KinectStreamsMat.cpp
+++ src/KinectStreamsMat.cpp
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  *
  */
-
+ 
 #include "KinectStreamsMat.h"
 
 CKinectStreamsMat::CKinectStreamsMat()
@@ -20,6 +20,8 @@ CKinectStreamsMat::CKinectStreamsMat()
 	_depth_source = nullptr;
 	_color_reader = nullptr;
 	_color_source = nullptr;
+	_ir_source = nullptr;
+	_ir_reader = nullptr;
 }
 
 CKinectStreamsMat::~CKinectStreamsMat()
@@ -28,13 +30,15 @@ CKinectStreamsMat::~CKinectStreamsMat()
 	SafeRelease(_depth_reader);
 	SafeRelease(_color_source);
 	SafeRelease(_color_reader);
+	SafeRelease(_ir_source);
+	SafeRelease(_ir_reader);
 	if (_sensor) {
 		_sensor->Close();
 	}
 	SafeRelease(_sensor);
 }
 
-cv:: Mat CKinectStreamsMat::getDepthFrame()
+cv::Mat CKinectStreamsMat::getDepthFrame()
 {
 	HRESULT hr = E_FAIL;
 	IDepthFrame* frame = nullptr;
@@ -53,9 +57,9 @@ cv:: Mat CKinectStreamsMat::getDepthFrame()
 			if (SUCCEEDED(hr)) {
 				const int imgSize = frameWidth*frameHeight;
 				UINT16* pixelData = new UINT16[imgSize];
-				hr = frame->CopyFrameDataToArray(imgSize,pixelData);
+				hr = frame->CopyFrameDataToArray(imgSize, pixelData);
 				if (SUCCEEDED(hr)) {
-					depthImage = cv::Mat(frameHeight,frameWidth, CV_8U);
+					depthImage = cv::Mat(frameHeight, frameWidth, CV_8U);
 					for (int i = 0; i < imgSize; i++) {
 						UINT16 depth = pixelData[i];
 						depthImage.at<UINT8>(i) = depth & 0xffff;
@@ -69,35 +73,86 @@ cv:: Mat CKinectStreamsMat::getDepthFrame()
 	return depthImage;
 }
 
- cv::Mat CKinectStreamsMat::getColorFrame()
- {
-	 HRESULT hr = E_FAIL;
-	 IColorFrame* frame = NULL;
-	 IFrameDescription* frameDesc;
+cv::Mat CKinectStreamsMat::getColorFrame()
+{
+	HRESULT hr = E_FAIL;
+	IColorFrame* frame = NULL;
+	IFrameDescription* frameDesc;
 	cv::Mat colorImage;
-	 hr = _color_reader->AcquireLatestFrame(&frame);
-	 if (SUCCEEDED(hr)) {
-		 hr = frame->get_FrameDescription(&frameDesc);
-		 if (SUCCEEDED(hr)) {
-			 int frameWidth = 0, frameHeight = 0;
-			 hr = frameDesc->get_Width(&frameWidth);
-			 if (SUCCEEDED(hr)) {
-				 hr = frameDesc->get_Height(&frameHeight);
-			 }
-			 if (SUCCEEDED(hr)) {
-				 const int imgSize = frameWidth*frameHeight * 4; //4 Channels(BGRA)
-				 BYTE* frameData = new BYTE[imgSize];
-				 hr = frame->CopyConvertedFrameDataToArray(imgSize, frameData, ColorImageFormat_Bgra);
-				 if (SUCCEEDED(hr)) {
-					 colorImage = cv::Mat(frameHeight, frameWidth, CV_8UC4, reinterpret_cast<void*>(frameData));
-				 }
-			 }
-		 }
-		 SafeRelease(frameDesc);
-		 SafeRelease(frame);
-	 }
-	 return colorImage;
- }
+	hr = _color_reader->AcquireLatestFrame(&frame);
+	if (SUCCEEDED(hr)) {
+		hr = frame->get_FrameDescription(&frameDesc);
+		if (SUCCEEDED(hr)) {
+			int frameWidth = 0, frameHeight = 0;
+			hr = frameDesc->get_Width(&frameWidth);
+			if (SUCCEEDED(hr)) {
+				hr = frameDesc->get_Height(&frameHeight);
+			}
+			if (SUCCEEDED(hr)) {
+				const int imgSize = frameWidth*frameHeight * 4; //4 Channels(BGRA)
+				BYTE* frameData = new BYTE[imgSize];
+				hr = frame->CopyConvertedFrameDataToArray(imgSize, frameData, ColorImageFormat_Bgra);
+				if (SUCCEEDED(hr)) {
+					colorImage = cv::Mat(frameHeight, frameWidth, CV_8UC4, reinterpret_cast<void*>(frameData));
+				}
+			}
+		}
+		SafeRelease(frameDesc);
+		SafeRelease(frame);
+	}
+	return colorImage;
+}
+
+cv::Mat CKinectStreamsMat::getIRframe()
+{
+	HRESULT hr = E_FAIL;
+	IInfraredFrame* frame = nullptr;
+	IFrameDescription* frameDesc;
+	cv::Mat infraredImage;
+	hr = _ir_reader->AcquireLatestFrame(&frame);
+	if (SUCCEEDED(hr)) {
+		hr = frame->get_FrameDescription(&frameDesc);
+		if (SUCCEEDED(hr)) {
+			int frameWidth = 0, frameHeight = 0;
+			hr = frameDesc->get_Width(&frameWidth);
+			if (SUCCEEDED(hr)) {
+				hr = frameDesc->get_Height(&frameHeight);
+			}
+			if (SUCCEEDED(hr)) {
+				const int imgSize = frameWidth*frameHeight;
+				UINT16* frameData = new UINT16[imgSize];
+				BYTE* frameDataBytes = new BYTE[imgSize];
+				hr = frame->CopyFrameDataToArray(imgSize,frameData);
+				for (int i = 0; i < imgSize; i++) {
+					frameDataBytes[i] = (BYTE)(frameData[i] >> 7);
+				}
+				if (SUCCEEDED(hr)) {
+					infraredImage = cv::Mat(frameHeight, frameWidth, CV_8U, reinterpret_cast<void*>(frameDataBytes));
+				}
+			}
+		}
+		SafeRelease(frameDesc);
+		SafeRelease(frame);
+	}
+	return infraredImage;
+
+}
+
+cv::Mat CKinectStreamsMat::getFrame(FRAME_TYPE TYPE)
+{
+	if (TYPE == KS_DEPTH) {
+		return getDepthFrame();
+	}
+	else if (TYPE == KS_IR) {
+		return getIRframe();
+	}
+	else if (TYPE == KS_COLOR) {
+		return getColorFrame();
+	}
+	else {
+		return cv::Mat();
+	}
+}
 
 HRESULT CKinectStreamsMat::initSensor()
 {
@@ -105,11 +160,11 @@ HRESULT CKinectStreamsMat::initSensor()
 	if (FAILED(hr)) {
 		return hr;
 	}
-	
+
 	if (_sensor) {
 		hr = _sensor->Open();
 		HRESULT hr2 = hr;
-
+		HRESULT hr3 = hr;
 		if (SUCCEEDED(hr)) {
 			hr = _sensor->get_DepthFrameSource(&_depth_source);
 		}
@@ -126,7 +181,20 @@ HRESULT CKinectStreamsMat::initSensor()
 			hr2 = _color_source->OpenReader(&_color_reader);
 		}
 
-		hr = hr2;
+		if (SUCCEEDED(hr3)) {
+			hr3 = _sensor->get_InfraredFrameSource(&_ir_source);
+		}
+		if (SUCCEEDED(hr3)) {
+			hr3 = _ir_source->OpenReader(&_ir_reader);
+		}
+		
+		if (FAILED(hr2)) {
+			return hr2;
+		}
+		
+		if (FAILED(hr3)) {
+			return hr3;
+		}
 	}
 
 	return hr;
